9078eb2c1100baadb5afb577f0dfdb9d2266ce57
LennyDzho <99424080+LennyDzho@users.noreply.github.com>
2023-04-20 02:14:08 +0300

    Реализация регистрации и авторизации, а
    ну и кнопки выхода

---
+++ regg/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <?php
+    $website_title = "CRUD";
+    require "blocks/head.php";
+    ?>
+</head>
+
+<body>
+    <?php require "blocks/header.php" ?>
+
+    <main>
+        <p>Main text</p>
+    </main>
+
+    <?php require "blocks/aside.php" ?>
+    <?php require "blocks/footer.php" ?>
+</body>
+
+</html>
